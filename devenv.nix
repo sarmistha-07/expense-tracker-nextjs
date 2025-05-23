4b0423fc8f2a9993b85505bb1dbe37a0a4a925b0
sarmistha-07 <sarmisthamarik13@gmail.com>
2025-05-24 00:28:29 +0530

    Initial commit

---
+++ devenv.nix
@@ -0,0 +1,54 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/languages/
+  # Provides `tsc`, `tsc test.ts` compiles TypeScript into JavaScript
+  # You can now use `node test.js` to run the compiled JavaScript file
+  languages.typescript.enable = true;
+
+  # Enables node and javascript support!
+  languages.javascript = {
+    enable = true;
+    # PNPM saves space and faster than Yarn classic and NPM!
+    pnpm.enable = true;
+  };
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
